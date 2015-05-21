c00f6a5a3d42b1f31ba654494bd48dc6eabc9fa2
Chee Vue <ctvue@yahoo.com>
2015-05-20 23:21:41 -0700

    Initial Commit

---
+++ HallofFramesUpdate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallofFramesUpdate
+//
+//  Created by Chee Vue on 5/20/15.
+//  Copyright (c) 2015 Chee Vue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
